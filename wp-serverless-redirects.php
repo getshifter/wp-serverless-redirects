04ee43d45a31c511a12033cd9453cbc818d49ba6
Daniel Olson <emaildano@gmail.com>
2018-08-30 16:22:26 -0400

    updated to use WP REST API

--- wp-serverless-redirects.php
+++ wp-serverless-redirects.php
@@ -7,28 +7,52 @@ Author URI: https://github.com/getshifter/wp-serverless-redirects
 Description: Redirects for static WordPress sites
 */
 
-function wp_sls_redir_enqueue_script() {   
-    wp_enqueue_script( 'wp_sls_redirects', plugin_dir_url( __FILE__ ) . 'js/wp-serverless-redirects.js' );
-}
+function compile_redirects(
+    $routes = array(
+        'redirection/v1/export/all/json'
+    )
+) {
 
-add_action('wp_head', 'wp_sls_redir_enqueue_script');
+    $redirects_array = array();
 
+    foreach ($routes as $route) {
+        
+        $url =  esc_url( home_url( '/' ) ) . 'wp-json/wp/v2/' . $route;
+        $jsonData = json_decode( file_get_contents($url) );
 
-function wp_sls_redir_export() {
-
-    $file = 'redirects.json';
-    $upload_dir = wp_get_upload_dir();
-    $save_path = $upload_dir['basedir'] . '/wp-sls-redirects';
-
-    if (!is_dir($save_path)) {
-        mkdir($save_path, 0755, true);
+        $redirects_array[$route] = (array) $jsonData;
     }
 
-    exec('wp redirection export all '. $file .' --format=json');
-    exec('mv ' . $file . ' ' . $save_path );
+    $redirects = json_encode($redirects_array);
+
+    return $redirects;
 
 }
 
-add_action( 'redirection_redirect_updated', 'wp_sls_redir_export' );
-add_action( 'redirection_save_options', 'wp_sls_redir_export' );
-add_action( 'redirection_redirect_deleted', 'wp_sls_redir_export' );
+function save_redirects(
+        $redirects,
+        $file_name = 'redirection.json'
+    ) {
+    $upload_dir = wp_get_upload_dir();
+    $save_path = $upload_dir['basedir'] . '/wp-sls-redirects/' . $file_name;
+    $dirname = dirname($save_path);
+    
+    if (!is_dir($dirname))
+    {
+        mkdir($dirname, 0755, true);
+    }
+
+    $f = fopen( $save_path , "w+" );
+    fwrite($f , $redirects);
+    fclose($f);
+}
+
+function build_redirects()
+{
+    $redirects = compile_redirects();
+    save_redirects($redirects);
+}
+
+add_action( 'redirection_redirect_updated', 'build_redirects' );
+add_action( 'redirection_save_options', 'build_redirects' );
+add_action( 'redirection_redirect_deleted', 'build_redirects' );
